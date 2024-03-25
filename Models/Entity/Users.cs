19765261cd8b492bc9adfae5782ff61b8f25e299
DESKTOP-3S5H9LP\clud1 <Sanbylai@gmail.com>
2024-03-25 16:26:18 +0800

    IIS 連到了，但docker一樣連不到資料庫

--- Models/Entity/Users.cs
+++ Models/Entity/Users.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime CreatDateTime { get; set; }
+        public DateTime CreateDateTime { get; set; }
         public DateTime UpdateTime { get; set;}
       }
 }
